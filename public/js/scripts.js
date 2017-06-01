f727d9c5b42a83c5e044f87a55ebb0d60855f951
Douglas Dantas de Souza <doug.souda@gmail.com>
2017-06-01 00:24:42 -0300

    Novo front-end

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Freelancer.", "I Design Websites.", "I Live in New York."],    /* You can change the home section typing text from
+            strings: ["Somos conhecidos como o Guia Gastronômico da cidade."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
